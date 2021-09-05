4419b5467becbf9c4d67814ecb7f968665839ad3
unknown <Aniket@ANIKET.Aniket>
2021-09-05 22:56:47 +0530

    Changed DB

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ const globalController = require('./controller/errorController');
 const mongoose = require('mongoose');
 const userRouter = require('./routes/userRoutes');
 
-// mongoose.connect('mongodb+srv://aniket:%40niket^1234@cluster0.irabd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(res => {
-//     console.log("Database Connected");
-// }).catch(err => {
-//     console.log("Error :",err);
-// })
-
-mongoose.connect("mongodb://127.0.0.1:27017/todoo",{
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-}).then(res => {
+mongoose.connect('mongodb+srv://aniket:%40niket^1234@cluster0.irabd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority').then(res => {
     console.log("Database Connected");
 }).catch(err => {
-    console.log(err);
+    console.log("Error :",err);
 })
+
+// mongoose.connect("mongodb://127.0.0.1:27017/todoo",{
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true
+// }).then(res => {
+//     console.log("Database Connected");
+// }).catch(err => {
+//     console.log(err);
+// })
 app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
